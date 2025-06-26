b0b57f35b420aadf7204a366b8b3d039fb5243de
Sabari Doss R <sabursd18@gmail.com>
2025-06-26 12:38:32 +0530

    feat : add app.js (main) backend

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./src/route/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
