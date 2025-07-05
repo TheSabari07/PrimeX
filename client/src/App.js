a165ef1f287e72dba5b3ee56e9dc7870fc8adf67
Sabari Doss R <sabursd18@gmail.com>
2025-07-05 22:50:52 +0530

    feat : updated app routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
 // Page imports
 import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Dashboard from "./pages/Dashboard";
+import Todo from "./pages/Todo"; 
 
 // utils 
 import ProtectedRoute from "./utils/ProtectedRoute";
@@ -23,6 +23,14 @@ const App = () => {
           element={
             <ProtectedRoute>
               <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/todo"
+          element={
+            <ProtectedRoute>
+              <Todo />
             </ProtectedRoute>
           }
         />
