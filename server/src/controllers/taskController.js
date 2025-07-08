683bc4169292480441eb0c996a9c1072e6154e68
Sabari Doss R <sabursd18@gmail.com>
2025-07-08 22:24:54 +0530

    feat(backend) : add controller for update and delete

--- server/src/controllers/taskController.js
+++ server/src/controllers/taskController.js
@@ -1,17 +1,33 @@
 import Task from "../models/Task.js";
 
-export const getTasks  = async (req,res) => {
-    const tasks = await Task.find({ userId: req.user.userId });
-    res.json(tasks);
-}
+export const getTasks = async (req, res) => {
+  const tasks = await Task.find({ userId: req.user.userId });
+  res.json(tasks);
+};
 
 export const createTask = async (req, res) => {
-    const { text, tags, deadline } = req.body;
-    const task = new Task({
-        userId: req.user.userId,
-        text,
-        tags,
-        deadline
-    });
-    res.status(201).json(task);
-}
+  const { text, tags, deadline } = req.body;
+  const task = await Task.create({
+    userId: req.user.userId,
+    text,
+    tags,
+    deadline,
+  });
+  res.status(201).json(task);
+};
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const updated = await Task.findOneAndUpdate(
+    { _id: id, userId: req.user.userId },
+    req.body,
+    { new: true }
+  );
+  res.json(updated);
+};
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  await Task.findOneAndDelete({ _id: id, userId: req.user.userId });
+  res.status(204).end();
+};
